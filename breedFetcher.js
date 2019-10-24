a0591dbb59b596c74bca71146b70cd113160e043
Zhabskyi <poltavagaz@gmail.com>
2019-10-24 20:38:13 +0000

    Refactor breadFetcher function and implement test for it

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,25 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+ 
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, res, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      if (body.length > 2 && res.statusCode === 200) {
+          error = null;
+          const data = JSON.parse(body);
+          const desc = data[0].description
+          callback(null, desc);
+        } else if (body.length === 2) {
+          callback(error);
+          //console.log("Requested breed is not found");
+        } else if (res.statusCode === 404) {
+          callback(error);
+          //console.log("File not found. Code 404");
+      }
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-    throw Error;
-  } else if (body.length > 2 && response.statusCode === 200) {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  } else if (body.length === 2) {
-    console.log("Requested breed is not found");
-  } else if (response.statusCode === 404) {
-    console.log("File not found. Code 404");
-  }
-});
+module.exports =  {fetchBreedDescription};
